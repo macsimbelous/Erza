1512f4470239a241f08244492c557b0757c76b0e
macsimbelous <macsimbelous@gmail.com>
2015-04-12 10:43:57 +0800

    PNG

--- Lucy/Program.cs
+++ Lucy/Program.cs
@@ -24,7 +24,7 @@ namespace Lucy
             {
                 tag = args[0];
             }
-            string prefix_path1 = "d:\\pictures\\animeart\\unsorted\\";
+            string prefix_path1 = "d:\\pictures\\animeart\\PNG\\";
             //string prefix_path2 = "d:\\pictures\\animeart\\художники\\";
             string dest_path = "d:\\temp\\";
             List<string> imgs = new List<string>();
