dff1e17edb5e81946b8ffcd04eb930a49d1228b1
maksim <maksim@ANEW>
2017-06-13 13:12:04 +0800

    Maki 2

--- Maki/Program.cs
+++ Maki/Program.cs
@@ -21,17 +21,56 @@ namespace Maki
             SQLiteConnection conn = new SQLiteConnection(previews);
             conn.Open();
             string[] files = Directory.GetFiles("I:\\AnimeArt", "*.*", SearchOption.AllDirectories);
-            foreach(string file in files)
+            ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+            EncoderParameter myEncoderParameter = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 80L);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+            SQLiteTransaction transact = conn.BeginTransaction();
+            foreach (string file in files)
             {
-                if(ExistPreview(Path.GetFileNameWithoutExtension(file), conn))
+                string hash = Path.GetFileNameWithoutExtension(file);
+                if (ExistPreview(hash, conn))
                 {
                     Console.WriteLine(file + " Уже есть в БД");
                 }
                 else
                 {
                     Bitmap preview = CreateThumbnail(file, 200, 150);
-                    
+                    using(MemoryStream stream = new MemoryStream())
+                    {
+                        preview.Save(stream, jpgEncoder, myEncoderParameters);
+                        LoadPreviewToDB(hash, stream.ToArray(), conn);
+                    }
+                    Console.WriteLine(file + " Добавлен");
                 }
+            }
+            transact.Commit();
+            List<string> hashs = ReadAllHashFromPrewiewsDB(conn);
+            Console.WriteLine();
+            string erzadb = "data source=C:\\utils\\erza\\erza.sqlite";
+            using (SQLiteConnection erza_conn = new SQLiteConnection(erzadb))
+            {
+                erza_conn.Open();
+                transact = conn.BeginTransaction();
+                foreach (string hash in hashs)
+                {
+                    ImageInfo img = ErzaDB.GetImageWithOutTags(hash, erza_conn);
+                    if (img.IsDeleted)
+                    {
+                        RomovePreviewFromDB(hash, conn);
+                        Console.WriteLine("{0} Удалён!", hash);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} Присутствует!", hash);
+                    }
+                }
+                transact.Commit();
+            }
+            using (SQLiteCommand command = conn.CreateCommand())
+            {
+                command.CommandText = "vacuum;";
+                command.ExecuteNonQuery();
             }
             conn.Close();
         }
@@ -96,6 +135,59 @@ namespace Maki
             }
 
             return bmpOut;
+        }
+        public static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+        public static void LoadPreviewToDB(string Hash, byte[] Preview, SQLiteConnection Connection)
+        {
+            using (SQLiteCommand command = new SQLiteCommand(Connection))
+            {
+                command.CommandText = "INSERT INTO previews(hash, preview) VALUES(@hash, @preview);";
+                command.Parameters.AddWithValue("hash", Hash);
+                command.Parameters.AddWithValue("preview", Preview);
+                command.ExecuteNonQuery();
+            }
+        }
+        public static List<string> ReadAllHashFromPrewiewsDB(SQLiteConnection Connection)
+        {
+            List<string> hashs = new List<string>();
+            using (SQLiteCommand command = new SQLiteCommand(Connection))
+            {
+                command.CommandText = "SELECT hash FROM previews";
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    int count = 0;
+                    while (reader.Read())
+                    {
+                        string hash = (string)reader["hash"];
+                        hashs.Add(hash);
+                        count++;
+                        Console.Write("Считано: {0:##########}\r", count);
+                    }
+                }
+            }
+            return hashs;
+        }
+        public static void RomovePreviewFromDB(string Hash, SQLiteConnection Connection)
+        {
+            using (SQLiteCommand command = new SQLiteCommand(Connection))
+            {
+                command.CommandText = "DELETE FROM previews WHERE hash = @hash";
+                command.Parameters.AddWithValue("hash", Hash);
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
