d4c44ebb8dbfa26f58ac54fc9332bc19000d7fd0
maksim <maksim@ANEW>
2017-07-07 13:00:36 +0800

    wqe

--- Eris/Form1.cs
+++ Eris/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Eris
 {
     public partial class Form1 : Form
     {
+        SQLiteDataAdapter adapter;
+        DataTable table;
+        SQLiteConnection connection;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dataGridView1.AutoGenerateColumns = true;
+            table = new DataTable();
+            connection = new SQLiteConnection(@"data source=C:\utils\Erza\Erza.sqlite");
+            connection.Open();
+            adapter = new SQLiteDataAdapter();
+            adapter.SelectCommand = new SQLiteCommand("SELECT * FROM tags", connection);
+            SQLiteCommandBuilder builder = new SQLiteCommandBuilder(adapter);
+            adapter.Fill(this.table);
+            adapter.UpdateCommand = builder.GetUpdateCommand();
+            adapter.InsertCommand = builder.GetInsertCommand();
+            adapter.DeleteCommand = builder.GetDeleteCommand();
+            adapter.Update(this.table);
+            this.dataGridView1.DataSource = table;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            adapter.Update(this.table);
+            connection.Clone();
         }
     }
 }
