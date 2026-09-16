0b82f980f0cc112d6413d53db76e54944047bdd5
maksim <maksim@BRONYA>
2026-09-16 08:09:11 +0800

    Перевёл на .NET 10

--- Ange2/ViewImageForm.Designer.cs
+++ Ange2/ViewImageForm.Designer.cs
@@ -57,10 +57,10 @@
             // 
             pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
             pictureBox1.ContextMenuStrip = contextMenuStrip1;
-            pictureBox1.Location = new System.Drawing.Point(334, 14);
-            pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            pictureBox1.Location = new System.Drawing.Point(477, 23);
+            pictureBox1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new System.Drawing.Size(957, 664);
+            pictureBox1.Size = new System.Drawing.Size(1367, 1107);
             pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -71,38 +71,38 @@
             contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { viewinfullscreenToolStripMenuItem, edittagsToolStripMenuItem, nextToolStripMenuItem, prevToolStripMenuItem, deleteToolStripMenuItem });
             contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new System.Drawing.Size(152, 114);
+            contextMenuStrip1.Size = new System.Drawing.Size(200, 164);
             // 
             // viewinfullscreenToolStripMenuItem
             // 
             viewinfullscreenToolStripMenuItem.Name = "viewinfullscreenToolStripMenuItem";
-            viewinfullscreenToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            viewinfullscreenToolStripMenuItem.Size = new System.Drawing.Size(199, 32);
             viewinfullscreenToolStripMenuItem.Text = "На весь экран";
             viewinfullscreenToolStripMenuItem.Click += viewinfullscreenToolStripMenuItem_Click;
             // 
             // edittagsToolStripMenuItem
             // 
             edittagsToolStripMenuItem.Name = "edittagsToolStripMenuItem";
-            edittagsToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            edittagsToolStripMenuItem.Size = new System.Drawing.Size(199, 32);
             // 
             // nextToolStripMenuItem
             // 
             nextToolStripMenuItem.Name = "nextToolStripMenuItem";
-            nextToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            nextToolStripMenuItem.Size = new System.Drawing.Size(199, 32);
             nextToolStripMenuItem.Text = "Следуюшее";
             nextToolStripMenuItem.Click += nextToolStripMenuItem_Click;
             // 
             // prevToolStripMenuItem
             // 
             prevToolStripMenuItem.Name = "prevToolStripMenuItem";
-            prevToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            prevToolStripMenuItem.Size = new System.Drawing.Size(199, 32);
             prevToolStripMenuItem.Text = "Предыдушее";
             prevToolStripMenuItem.Click += prevToolStripMenuItem_Click;
             // 
             // deleteToolStripMenuItem
             // 
             deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            deleteToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            deleteToolStripMenuItem.Size = new System.Drawing.Size(199, 32);
             deleteToolStripMenuItem.Text = "Удалить";
             deleteToolStripMenuItem.Click += deleteToolStripMenuItem_Click;
             // 
@@ -111,11 +111,11 @@
             listBox1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 204);
             listBox1.FormattingEnabled = true;
-            listBox1.Location = new System.Drawing.Point(14, 14);
-            listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            listBox1.Location = new System.Drawing.Point(20, 23);
+            listBox1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             listBox1.Name = "listBox1";
             listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            listBox1.Size = new System.Drawing.Size(314, 504);
+            listBox1.Size = new System.Drawing.Size(447, 816);
             listBox1.TabIndex = 1;
             listBox1.DrawItem += listBox1_DrawItem;
             listBox1.DoubleClick += listBox1_DoubleClick;
@@ -128,11 +128,11 @@
             groupBox1.Controls.Add(resolution_label);
             groupBox1.Controls.Add(format_label);
             groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            groupBox1.Location = new System.Drawing.Point(14, 576);
-            groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            groupBox1.Location = new System.Drawing.Point(20, 960);
+            groupBox1.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            groupBox1.Size = new System.Drawing.Size(314, 101);
+            groupBox1.Padding = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            groupBox1.Size = new System.Drawing.Size(449, 168);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             groupBox1.Text = "Свойства";
@@ -141,9 +141,10 @@
             // 
             tags_count_label.AutoSize = true;
             tags_count_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            tags_count_label.Location = new System.Drawing.Point(7, 74);
+            tags_count_label.Location = new System.Drawing.Point(10, 123);
+            tags_count_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             tags_count_label.Name = "tags_count_label";
-            tags_count_label.Size = new System.Drawing.Size(44, 16);
+            tags_count_label.Size = new System.Drawing.Size(64, 25);
             tags_count_label.TabIndex = 3;
             tags_count_label.Text = "label3";
             // 
@@ -151,9 +152,10 @@
             // 
             size_label.AutoSize = true;
             size_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            size_label.Location = new System.Drawing.Point(7, 56);
+            size_label.Location = new System.Drawing.Point(10, 93);
+            size_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             size_label.Name = "size_label";
-            size_label.Size = new System.Drawing.Size(44, 16);
+            size_label.Size = new System.Drawing.Size(64, 25);
             size_label.TabIndex = 2;
             size_label.Text = "label3";
             // 
@@ -161,9 +163,10 @@
             // 
             resolution_label.AutoSize = true;
             resolution_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            resolution_label.Location = new System.Drawing.Point(7, 37);
+            resolution_label.Location = new System.Drawing.Point(10, 62);
+            resolution_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             resolution_label.Name = "resolution_label";
-            resolution_label.Size = new System.Drawing.Size(44, 16);
+            resolution_label.Size = new System.Drawing.Size(64, 25);
             resolution_label.TabIndex = 1;
             resolution_label.Text = "label2";
             // 
@@ -171,9 +174,10 @@
             // 
             format_label.AutoSize = true;
             format_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            format_label.Location = new System.Drawing.Point(7, 19);
+            format_label.Location = new System.Drawing.Point(10, 32);
+            format_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             format_label.Name = "format_label";
-            format_label.Size = new System.Drawing.Size(44, 16);
+            format_label.Size = new System.Drawing.Size(64, 25);
             format_label.TabIndex = 0;
             format_label.Text = "label1";
             // 
@@ -181,10 +185,10 @@
             // 
             AddTag_button.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             AddTag_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            AddTag_button.Location = new System.Drawing.Point(14, 536);
-            AddTag_button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            AddTag_button.Location = new System.Drawing.Point(20, 893);
+            AddTag_button.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             AddTag_button.Name = "AddTag_button";
-            AddTag_button.Size = new System.Drawing.Size(74, 32);
+            AddTag_button.Size = new System.Drawing.Size(106, 53);
             AddTag_button.TabIndex = 3;
             AddTag_button.Text = "Добавить";
             AddTag_button.UseVisualStyleBackColor = true;
@@ -194,10 +198,10 @@
             // 
             RemoveTag_button.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             RemoveTag_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            RemoveTag_button.Location = new System.Drawing.Point(94, 536);
-            RemoveTag_button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            RemoveTag_button.Location = new System.Drawing.Point(134, 893);
+            RemoveTag_button.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             RemoveTag_button.Name = "RemoveTag_button";
-            RemoveTag_button.Size = new System.Drawing.Size(73, 32);
+            RemoveTag_button.Size = new System.Drawing.Size(104, 53);
             RemoveTag_button.TabIndex = 4;
             RemoveTag_button.Text = "Удалить";
             RemoveTag_button.UseVisualStyleBackColor = true;
@@ -207,10 +211,10 @@
             // 
             Search_button.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             Search_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 204);
-            Search_button.Location = new System.Drawing.Point(222, 536);
-            Search_button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Search_button.Location = new System.Drawing.Point(317, 893);
+            Search_button.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             Search_button.Name = "Search_button";
-            Search_button.Size = new System.Drawing.Size(60, 32);
+            Search_button.Size = new System.Drawing.Size(86, 53);
             Search_button.TabIndex = 6;
             Search_button.Text = "Найти";
             Search_button.UseVisualStyleBackColor = true;
@@ -220,18 +224,21 @@
             // 
             add_to_favorited_button.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
             add_to_favorited_button.Image = (System.Drawing.Image)resources.GetObject("add_to_favorited_button.Image");
-            add_to_favorited_button.Location = new System.Drawing.Point(290, 536);
+            add_to_favorited_button.Location = new System.Drawing.Point(414, 893);
+            add_to_favorited_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             add_to_favorited_button.Name = "add_to_favorited_button";
-            add_to_favorited_button.Size = new System.Drawing.Size(35, 32);
+            add_to_favorited_button.Size = new System.Drawing.Size(50, 53);
             add_to_favorited_button.TabIndex = 7;
             add_to_favorited_button.UseVisualStyleBackColor = true;
             add_to_favorited_button.Click += add_to_favorited_button_Click;
             // 
             // button1
             // 
-            button1.Location = new System.Drawing.Point(173, 536);
+            button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            button1.Location = new System.Drawing.Point(247, 893);
+            button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(43, 32);
+            button1.Size = new System.Drawing.Size(61, 53);
             button1.TabIndex = 5;
             button1.Text = "ИИ";
             button1.UseVisualStyleBackColor = true;
@@ -239,9 +246,9 @@
             // 
             // ViewImageForm
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(1305, 692);
+            ClientSize = new System.Drawing.Size(1864, 1153);
             Controls.Add(button1);
             Controls.Add(add_to_favorited_button);
             Controls.Add(Search_button);
@@ -251,7 +258,7 @@
             Controls.Add(listBox1);
             Controls.Add(pictureBox1);
             Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
-            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             Name = "ViewImageForm";
             StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             Text = "Просмотр";
